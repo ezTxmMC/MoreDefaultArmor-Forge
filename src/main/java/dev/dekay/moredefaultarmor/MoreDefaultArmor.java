0ee673f9f766b290050fc6a54238d1be56844683
ezTxmMC <eztxmmc@hotmail.com>
2024-07-29 23:42:40 +0200

    started with UpdateChecker

--- src/main/java/dev/dekay/moredefaultarmor/MoreDefaultArmor.java
+++ src/main/java/dev/dekay/moredefaultarmor/MoreDefaultArmor.java
@@ -3,8 +3,13 @@ package dev.dekay.moredefaultarmor;
 import com.mojang.logging.LogUtils;
 import dev.dekay.moredefaultarmor.item.ModItemTabs;
 import dev.dekay.moredefaultarmor.item.ModItems;
+import dev.dekay.moredefaultarmor.util.UpdateChecker;
+import net.minecraft.network.chat.Component;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
@@ -16,19 +21,22 @@ public class MoreDefaultArmor {
 
     private static ModItems items;
 
+    private static UpdateChecker updateChecker;
+
     public MoreDefaultArmor() {
-        LOGGER.info("Starting eventbus...");
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        LOGGER.info("Started eventbus.");
-        LOGGER.info("Register eventbus...");
-        MinecraftForge.EVENT_BUS.register(this);
-        LOGGER.info("Registered eventbus.");
-        LOGGER.info("Register items...");
+        MinecraftForge.EVENT_BUS.register(eventBus);
         items = new ModItems(eventBus);
-        LOGGER.info("Registered items.");
         new ModItemTabs(eventBus);
-        LOGGER.info("Register itemtabs...");
-        LOGGER.info("Registered itemtabs.");
+        updateChecker = new UpdateChecker(ModLoadingContext.get().getActiveContainer().getModInfo().getVersion().getQualifier());
+        System.out.println(updateChecker.getLatestVersion());
+    }
+
+    @SubscribeEvent
+    public static void playerJoinWorld(PlayerEvent.PlayerLoggedInEvent event) {
+        if (updateChecker.latestVersion()) {
+            event.getEntity().sendSystemMessage(Component.translatable("message.moredefaultarmor.update"));
+        }
     }
 
     public static ModItems getItems() {
